bbfbaee6e80ad820ccd90f099795eb7f2e110834
hugogbs <hugo.silva@ccc.ufcg.edu.br>
2017-05-19 13:35:55 -0300

    continuação do script, mas ainda com problema na inserção dos dados

--- analises/merenda/variacao-preco/licitacoes-merenda.sql
+++ analises/merenda/variacao-preco/licitacoes-merenda.sql
@@ -1,3 +1,5 @@
+USE sagres;
+
 DROP TABLE IF EXISTS classificacao_licitacao;
 
 CREATE TABLE classificacao_licitacao(
@@ -14,12 +16,6 @@ ALTER TABLE classificacao_licitacao
     ADD CONSTRAINT fk_classificacao_licitacao FOREIGN KEY (cd_UGestora, nu_Licitacao, tp_Licitacao)
         REFERENCES licitacao (cd_UGestora, nu_Licitacao, tp_Licitacao);
 
-create temporary table em as 
-    select cd_ugestora, nu_licitacao, tp_licitacao, sum(vl_empenho) as soma 
-    from empenhos 
-    where cd_funcao = 12 
-        and (cd_subelemento = 02 or cd_SubFuncao = 306) 
-    group by cd_ugestora, nu_licitacao, tp_licitacao;
 
 create temporary table e as 
     select cd_ugestora, nu_licitacao, tp_licitacao, sum(vl_empenho) as soma 
@@ -27,5 +23,23 @@ create temporary table e as
     group by cd_ugestora, nu_licitacao, tp_licitacao;
 
 
-INSERT INTO classificacao_licitacao
-    SELECT e.cd_UGestora, e.nu_Licitacao, e.tp_Licitacao, 
+create temporary table em as 
+    select cd_ugestora, nu_licitacao, tp_licitacao, sum(vl_empenho) as soma 
+    from empenhos 
+    where cd_funcao = 12 
+        and (cd_subelemento = 02 or cd_SubFuncao = 306) 
+    group by cd_ugestora, nu_licitacao, tp_licitacao;
+
+
+create temporary table e1 as
+	select e.cd_ugestora, e.nu_licitacao, e.tp_licitacao, em.soma/e.soma as pr_EmpenhosMerenda
+	from e, em
+	where e.cd_ugestora = em.cd_ugestora AND e.nu_licitacao = em.nu_licitacao AND e.tp_licitacao = em.tp_licitacao;
+
+
+INSERT INTO classificacao_licitacao (cd_UGestora, nu_Licitacao, tp_Licitacao)
+	select e.cd_ugestora, e.nu_licitacao, e.tp_licitacao
+	from e;
+
+UPDATE classificacao_licitacao c
+	SET pr_EmpenhosMerenda = (select pr_EmpenhosMerenda from e1 where c.cd_ugestora = e1.cd_ugestora AND c.nu_licitacao = e1.nu_licitacao AND c.tp_licitacao = e1.tp_licitacao);
