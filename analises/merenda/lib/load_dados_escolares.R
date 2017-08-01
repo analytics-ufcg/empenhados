92151c365f69e758757f4c0dde0c9a06b13b41fe
Julio Cesar Neves <julio.cesar.silva@ccc.ufcg.edu.br>
2017-08-01 17:54:19 -0300

    Interpola dados de total de alunos

--- analises/merenda/lib/load_dados_escolares.R
+++ analises/merenda/lib/load_dados_escolares.R
@@ -6,45 +6,51 @@ load_dados_escolares <- function() {
   library(stringr)
   
   #Pasta de origem dos arquivos csv deve ser modificada aqui
-  file_names <- Sys.glob("../dados/dados_escolares_2015/*.csv")
+  file_names <- Sys.glob("../dados/dados_escolares/*/*.csv")
   
   dados <- data.frame()
   
   for (file_name in file_names) {
     
     dados_escolares <- read.csv(file_name, header=FALSE, fileEncoding="latin1", 
-                                sep = ";", dec = ",", stringsAsFactors = FALSE)
+                                sep = ";", dec = ",", stringsAsFactors = FALSE, na.strings = c("Não informado", "Não existente"))
     
-    dados_escolares = dados_escolares %>%
+    path_chunks <- str_split(file_name, "/")[[1]]
+    
+    dados_escolares <- dados_escolares %>%
       select(c(V1, V2)) %>%
       mutate(cidade = dados_escolares$V1 [1]) %>%
       mutate(cidade = as.factor(cidade)) %>%
-      mutate(ano = 2015) %>%
-      filter(grepl('municipal', dados_escolares$V1, ignore.case = T)) %>%
+      mutate(ano = as.integer(path_chunks[4])) %>%
+      filter(grepl('municipal', dados_escolares$V1, ignore.case = T)) 
+    
+    dados_escolares <- dados_escolares %>%
+      filter(!grepl('superior', dados_escolares$V1, ignore.case = T)) %>%
       mutate(V2 = str_replace_all(V2, '\\.', '')) %>%
       mutate(V2 = as.integer(V2)) %>%
       spread(V1, V2)
     
-    codcid = str_replace(file_name, 'dados_escolares_2015/', '')
-    codcid = str_sub(codcid, end = 7)
-    dados_escolares = dados_escolares %>%
-      mutate(codcidade = as.integer(codcid))
+    codcid <- str_sub(path_chunks[5], end = 6)
+    
+    dados_escolares <- dados_escolares %>%
+      mutate(codcidade = codcid)
+    
+    colnames(dados_escolares) <- 
+      c("de_Municipio","dt_Ano", "vl_Docentes_Fundamental", "vl_Docentes_Medio", "vl_Docentes_Pre_Escolar", 
+        "vl_Escolas_Fundamental", "vl_Escolas_Medio", "vl_Escolas_Pre_Escolar", 
+        "vl_Matriculas_Fundamental", "vl_Matriculas_Medio", "vl_Matriculas_Pre_Escolar", "cd_IBGE")
     
     dados <- rbind(dados, dados_escolares)
   }
   
-  colnames(dados) <- c("cidade","ano", "docfund", "docmedio", "docpre", "escfund", 
-                      "escmedio", "escpre", "matrifund", "matrimedio", "matripre", "codcidade")
-  
   dados <- dados %>%
-     mutate(doctotal = docfund + docmedio + docpre) %>%
-     mutate(esctotal = escfund + escmedio + escpre) %>%
-     mutate(matritotal = matrifund + matrimedio + matripre)
+     mutate(vl_Docentes_Total = vl_Docentes_Fundamental + vl_Docentes_Medio + vl_Docentes_Pre_Escolar) %>%
+     mutate(vl_Escolas_Total = vl_Escolas_Fundamental + vl_Escolas_Medio + vl_Escolas_Pre_Escolar) %>%
+     mutate(vl_Matriculas_Total = vl_Matriculas_Fundamental + vl_Matriculas_Medio + vl_Matriculas_Pre_Escolar)
   
-  dados = dados[c("cidade","codcidade", "ano", 
-                  "docfund", "docmedio", "docpre", "doctotal",
-                  "escfund", "escmedio", "escpre", "esctotal", 
-                  "matrifund", "matrimedio", "matripre", "matritotal")]
+  dados <- dados[c("cd_IBGE", "de_Municipio", "dt_Ano", "vl_Docentes_Fundamental", "vl_Docentes_Medio", "vl_Docentes_Pre_Escolar", 
+                  "vl_Escolas_Fundamental", "vl_Escolas_Medio", "vl_Escolas_Pre_Escolar", 
+                  "vl_Matriculas_Fundamental", "vl_Matriculas_Medio", "vl_Matriculas_Pre_Escolar")]
   
   return(dados)
 }
