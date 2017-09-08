4011d9312f24f5da0c2aedf77edb1471db69a9ab
gileadekelvin <gileadekelvin@gmail.com>
2017-09-08 15:11:12 -0300

    Alteração da aplicação considerando apenas as descrições que são do NCM selecionado

--- analises/geral/notas_fiscais/nfe_filter.R
+++ analises/geral/notas_fiscais/nfe_filter.R
@@ -3,13 +3,13 @@ library(readr)
 
 notas <-  src_mysql('notas_fiscais', group='ministerio-publico', password=NULL)
 
-nfe <- tbl(src_mysql('notas_fiscais', group='ministerio-publico', password=NULL), 'nota_fiscal_melhor') %>%
+nfe_bd <- tbl(src_mysql('notas_fiscais', group='ministerio-publico', password=NULL), 'nota_fiscal') %>%
   #select(Chave_de_acesso, Nr_item, Descricao_do_Produto_ou_servicos, NCM_prod, Unid_prod) %>%
   collect(n = Inf)
 
-write.csv(nfe, "nfe_small.csv", row.names = FALSE)
+write_csv(nfe_bd, "nfe_small.csv")
 
-nfe_csv <- read.csv("nfe_small.csv", encoding = "utf8")
+nfe_csv <- read.csv("nfe_small.csv", stringsAsFactors = FALSE,  colClasses=c("Chave_de_acesso"="character"))
 
 
 
--- analises/geral/notas_fiscais/nfe_topWords.R
+++ analises/geral/notas_fiscais/nfe_topWords.R
@@ -4,7 +4,7 @@ library(readr)
 library(tm)
 options(scipen = 999)
 
-nfe <- read_csv("notas-fiscais-refined.csv")
+nfe <- read.csv("notas-fiscais-refined.csv", stringsAsFactors = FALSE,  colClasses=c("Chave_de_acesso"="character"), encoding = "utf8")
 
 word.count <- function(data) {
 
@@ -28,12 +28,10 @@ word.clean <- function(descricao) {
     return()
 }
 
-nfe <- nfe %>%
-  mutate(id = seq(nrow(nfe)))
-
 nfe.split <- nfe %>%
   select(
-    id,
+    Chave_de_acesso,
+    Nr_item,
     NCM = NCM_prod,
     descricao = Descricao_do_Produto_ou_servicos) %>%
   mutate(
@@ -43,15 +41,15 @@ nfe.split <- nfe %>%
 metrica <- nfe.split %>%
   group_by(NCM) %>%
   mutate(percent = word.count(descricao)) %>%
-  group_by(id) %>%
+  group_by(Chave_de_acesso,
+            Nr_item) %>%
   distinct(descricao, .keep_all = TRUE) %>% 
   summarise(metrica = sum(percent, na.rm = TRUE))
 
 nfe.result <- nfe %>%
-  left_join(metrica, by = "id") %>% 
+  left_join(metrica, by = c("Chave_de_acesso",
+                             "Nr_item")) %>% 
   replace_na(list(metrica = 0)) %>%
   rename(Metrica = metrica) %>% 
-  select(-c(id, NCM_prod, Descricao_do_Produto_ou_servicos, Unid_prod))
-
-
+  select(-c(NCM_prod, Descricao_do_Produto_ou_servicos))
 
