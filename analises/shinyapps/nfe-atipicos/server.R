d2865904ef0433cd6e0ba59406113fedd66549e1
gileadekelvin <gileadekelvin@gmail.com>
2017-09-13 14:53:06 -0300

    Determina se uma descrição pertence ao NCM a através do agrupamento

--- analises/shinyapps/nfe-atipicos/server.R
+++ analises/shinyapps/nfe-atipicos/server.R
@@ -1,6 +1,6 @@
 server <- function(input, output, session){
   set.seed(123)
-  options(warn = -1)
+  #options(warn = -1)
   
   ncm_input <- reactive({
     str_sub(input$busca, 1, 8)
@@ -11,7 +11,7 @@ server <- function(input, output, session){
     
     template <- ('
     SELECT Valor_unit_prod, NCM_prod, Descricao_do_Produto_ou_servicos, Nome_razao_social_emit, Nome_razao_social_dest, 
-                  Valor_total_da_nota, Data_de_emissao, Unid_prod, Metrica
+                  Valor_total_da_nota, Data_de_emissao, Unid_prod, Metrica, Confiavel
     FROM nota_fiscal
     WHERE NCM_prod = "%s"
   ')
@@ -20,11 +20,9 @@ server <- function(input, output, session){
       sprintf(ncm_input) %>%
       sql()
     
-    nfe <- tbl(src_mysql('notas_fiscais', group='ministerio-publico', password=NULL), query) %>% 
+    nfe <- tbl(src_mysql('notas_fiscais', group='ministerio-publico', password=NULL), query) %>%
       collect(n = Inf) %>% 
-      rowwise() %>% 
-      mutate(isNCM = sample(c(TRUE, FALSE), size = 1, replace = TRUE, prob = c(Metrica, 1 - Metrica))) %>% 
-      ungroup()
+      mutate(Confiavel = ifelse(Confiavel == "TRUE", TRUE, FALSE))
       
   })  
   
@@ -38,16 +36,15 @@ server <- function(input, output, session){
   output$scatter <- renderPlotly({
     
     dados_nfe <- dados_nfe() %>%
-      filter(Unid_prod == input$select_unid, isNCM == TRUE)
+      filter(Unid_prod == input$select_unid, Confiavel == TRUE)
 
     quantile = quantile(dados_nfe$Valor_unit_prod, probs = c(0.9))
 
     if (nrow(dados_nfe) != 0) {
       dados_nfe %>%
-        mutate(dummy = seq(nrow(dados_nfe)), 
-               atipico = ifelse(Valor_unit_prod > quantile, "Atípico", "Típico"),
+        mutate(atipico = ifelse(Valor_unit_prod > quantile, "Atípico", "Típico"),
                Metrica = round(Metrica, 3)) %>%
-        plot_ly(x = ~dummy, y = ~Valor_unit_prod, type = "scatter", mode = "markers", color = ~atipico, colors = "Set1",
+        plot_ly(x = ~Data_de_emissao, y = ~Valor_unit_prod, type = "scatter", mode = "markers", color = ~atipico, colors = "Set1",
                 text = ~paste('Descrição: ', Descricao_do_Produto_ou_servicos,
                               '<br>Emitente: ', Nome_razao_social_emit,
                               '<br>Destino: ', Nome_razao_social_dest,
@@ -67,8 +64,8 @@ server <- function(input, output, session){
   })
   
   output$table <- renderDataTable(arrange(dados_nfe() %>% 
-                                            filter(Unid_prod == input$select_unid, isNCM == FALSE) %>% 
-                                            select(-c(Unid_prod, NCM_prod, isNCM)), 
+                                            filter(Unid_prod == input$select_unid, Confiavel == FALSE) %>% 
+                                            select(-c(Unid_prod, NCM_prod)), 
                                           desc(Metrica)),
                                   options = list(scrollX = TRUE, pageLength = 10))
 
